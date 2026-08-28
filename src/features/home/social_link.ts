9813db8eaefc3fdbae1bea6971bc0c4e03fa26f2
Toluwalope Oluyipe <kingtoluwalope@gmail.com>
2026-08-28 13:32:24 -0400

    feat: add socialLinks array for social media integration

---
+++ src/features/home/social_link.ts
@@ -0,0 +1,12 @@
+import {
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,FaGoogle
+} from "react-icons/fa";
+
+export const socialLinks = [
+    { label: "Email", href: "mailto:youremail@example.com", Icon: FaGoogle },
+    { label: "LinkedIn", href: "https://www.linkedin.com/in/blazeken/", Icon: FaLinkedin },
+  { label: "GitHub", href: "https://github.com/blayziken", Icon: FaGithub },
+  { label: "Instagram", href: "https://www.instagram.com/blaze.jt/", Icon: FaInstagram },
+];

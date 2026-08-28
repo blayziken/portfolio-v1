80562b76054021fb815b2ab6b20af756159b89b8
Toluwalope Oluyipe <kingtoluwalope@gmail.com>
2026-08-28 13:32:31 -0400

    feat: add navItems array for navigation links

---
+++ src/features/home/nav_items.ts
@@ -0,0 +1,5 @@
+export const  navItems = [
+  { label: 'About', href: '#about', id: 'about' },
+  { label: 'Experience', href: '#experience', id: 'experience' },
+  { label: 'Projects', href: '#projects', id: 'projects' },
+]
